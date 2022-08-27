2e37eed734633c189e7c6bf9dae1982686f49566
mk-singhal <maniksingal29@gmail.com>
2022-08-27 20:00:59 +0530

    Added Shopping Cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Cart
+CART_SESSION_ID = 'cart'
